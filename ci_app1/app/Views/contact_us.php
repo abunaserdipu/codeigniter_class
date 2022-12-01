d61159dc0b56acef38c7c2c468b7948a1539d8bc
abunaserdipu <abunaserdipu0@gmail.com>
2022-12-01 19:01:02 +0600

    added first codeIgniter app

---
+++ ci_app1/app/Views/contact_us.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Contact Us</h1>
+    <form action="">
+        <label for="">Email:</label>
+        <input type="email" name="" id=""><br><br>
+        <label for="">Message:</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    </form>
+</body>
+
+</html>
