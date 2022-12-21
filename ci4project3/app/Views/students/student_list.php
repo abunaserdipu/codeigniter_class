6dca5301aa91ef1038bf9f2cd06f0f1fb42ff7f7
abunaserdipu <abunaserdipu0@gmail.com>
2022-12-21 19:02:52 +0600

    show data from database with table view

--- ci4project3/app/Views/students/student_list.php
+++ ci4project3/app/Views/students/student_list.php
@@ -2,9 +2,27 @@
 echo view("includes/navbar"); ?>
 <div class="container">
     <h1>All Student List</h1>
-    <?php foreach ($students as $student) {
-        echo $student['name'] . "<br>";
-    } ?>
+    <table style="width:50%">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+            </tr>
+        </thead>
+        <?php foreach ($students as $student) { ?>
+            <tbody>
+                <tr>
+                    <td><?= $student['id'] ?></td>
+                    <td><?= $student['name'] ?></td>
+                    <td><?= $student['email'] ?></td>
+                    <td><?= $student['address'] ?></td>
+                </tr>
+            </tbody>
+
+        <?php } ?>
+    </table>
 </div>
 
 <?php echo view("includes/footer"); ?>
